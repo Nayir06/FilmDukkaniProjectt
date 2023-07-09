d775a0aadd6819b105b329a3f4e569322ea5c230
Nayir06 <129368367+Nayir06@users.noreply.github.com>
2023-07-09 20:20:49 +0300

    .

---
+++ FilmDükkanı/FilmDükkanı.Entity/Enum/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmDükkanı.Entity.Enum
+{
+    internal class Status
+    {
+    }
+}
