6550de84e19b6d3f66a1dd5ee789829ef0caab88
Nayir06 <129368367+Nayir06@users.noreply.github.com>
2023-08-01 20:40:23 +0300

    .

--- FilmDükkanı/FilmDükkanı.MVC/Program.cs
+++ FilmDükkanı/FilmDükkanı.MVC/Program.cs
@@ -5,52 +5,85 @@ using FýlmDukkaný.BLL.Service;
 using FilmDukkaný.DAL.Context;
 using FilmDükkaný.Entity.Base;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.Design;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<FýlmDukkanýContext>();
+
+builder.Services.AddDbContext<FýlmDukkanýContext>(options => options.UseSqlServer(connectionString));
+
+//Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<FýlmDukkanýContext>().AddDefaultTokenProviders();
+
 
 //servisler
 
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<FýlmDukkanýContext>();
 
-builder.Services.AddTransient(typeof(IRepository<>), typeof(BaseRepository<>));
-builder.Services.AddScoped<ICategoryService, CategoryService>();
+//Cookie
+builder.Services.ConfigureApplicationCookie(x =>
+{
+x.LoginPath = new PathString("/Home/Login");
+x.AccessDeniedPath = new PathString("/Home/Login");
+x.Cookie = new CookieBuilder
+{
+Name = "loginCookie"
+};
+x.SlidingExpiration = true;
+x.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+});
+
+//Services
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+app.UseExceptionHandler("/Home/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
+
 app.UseRouting();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
-
+app.UseSession();
 
 
 app.UseEndpoints(endpoints =>
 {
 
-    endpoints.MapDefaultControllerRoute();//default buda
+    //Admin
 
-
-    endpoints.MapControllerRoute(   //admin paneli
+    endpoints.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
-});
 
+
+
+
+
+    //Default
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+});
 app.Run();
--- FilmDükkanı/FilmDükkanı.MVC/Utils/SessionHelper.cs
+++ FilmDükkanı/FilmDükkanı.MVC/Utils/SessionHelper.cs
@@ -1,6 +1,37 @@
-﻿namespace FilmDükkanı.MVC.Utils
+﻿using Newtonsoft.Json;
+
+namespace FilmDükkanı.MVC.Utils
 {
-    public class SessionHelper
+    public static class SessionHelper
     {
+        public static void SetJsonProduct(this ISession _session, string _key, object _value)
+        {
+            _session.SetString(_key, JsonConvert.SerializeObject(_value));
+        }
+
+
+        //Get
+        public static T GetProductFromJson<T>(this ISession session, string key)
+        {
+            var result = session.GetString(key);
+
+            if (result == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                var deserialize = JsonConvert.DeserializeObject<T>(result);
+                return deserialize;
+            }
+
+        }
+
+        //Remove
+
+        public static void RemoveSession(this ISession session, string key)
+        {
+            session.Remove(key);
+        }
     }
 }
