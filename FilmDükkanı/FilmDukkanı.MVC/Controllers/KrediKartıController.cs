53a9d2909662aba4e78980a0439f7b5ac7a0c0d8
Nayir06 <129368367+Nayir06@users.noreply.github.com>
2023-08-10 19:02:43 +0300

    .

---
+++ FilmDükkanı/FilmDukkanı.MVC/Controllers/KrediKartıController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDukkanı.MVC.Controllers
+{
+    public class KrediKartıController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FilmDükkanı/FilmDukkanı.MVC/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDukkanı.MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
